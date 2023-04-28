fa0bda3442e9762dad1cab1dc7867c19d1534166
= <=>
2023-04-27 19:06:09 -0500

    xz

--- books_new_life/settings.py
+++ books_new_life/settings.py
@@ -15,8 +15,8 @@ SECRET_KEY = "django-insecure-l*0m^l$$-al(4q)4@f7viykph)l51ep+xyrs+btjpuov5(j^fb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-CSRF_TRUSTED_ORIGINS = ['books-rvhd.onrender.com']
-ALLOWED_HOSTS = ['books-rvhd.onrender.com']
+#CSRF_TRUSTED_ORIGINS = ['books-rvhd.onrender.com']
+ALLOWED_HOSTS = ['books-rvhd.onrender.com','127.0.0.1']
 
 
 # ALLOWED_HOSTS = []
